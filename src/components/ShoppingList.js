d80ff0ffa4d8809db40d336226445104836ecaba
William Yu <wy427@nyu.edu>
2022-02-24 22:12:00 -0500

    completed Filter component deliverables

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,39 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearch(event) {
+    setSearchTerm(event.target.value);
+    console.log(event.target.value);
+  }
 
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items
+    .filter((item) => {
+      if (searchTerm == "") {
+        return item;
+      } else if (item.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return item;
+      }
+    })
+    .filter((item) => {
+      if (selectedCategory === "All") return true;
+
+      return item.category === selectedCategory;
+    });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearch}
+        search={searchTerm}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
